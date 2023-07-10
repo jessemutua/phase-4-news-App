c88b65da150a7b7902f6aae3ac1f37a049a0e348
Jesse <jessemutua76@gmail.com>
2023-07-10 05:24:29 +0300

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :authors
-  resources :categories
-  resources :articles
-  resources :users
+  resources :authors, only: [:index, show]
+  resources :categories, only: [:index, show]
+  resources :articles, only: [:index, :show, :create, :destroy]
+  resources :users, only:[:create, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
