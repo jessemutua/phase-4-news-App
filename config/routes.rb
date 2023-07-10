6b78b353681a2fb12ed28f2a3f188ba8d54149f2
Jesse <jessemutua76@gmail.com>
2023-07-10 09:18:10 +0300

    Corrected routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  resources :authors, only: [:index, show]
-  resources :categories, only: [:index, show]
+  resources :authors, only: [:index, :show]
+  resources :categories, only: [:index, :show]
   resources :articles, only: [:index, :show, :create, :destroy]
   resources :users, only:[:create, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :users, only: [:create]
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   get '/me', to: 'users#show'
